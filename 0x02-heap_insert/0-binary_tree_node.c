1ad56ff80ed1c327918e88fe2c9b07ee8dcbc6e7
natyarteagac <natalia.arteagac94@gmail.com>
2021-12-23 11:31:06 -0500

    Fixing betty style

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 
+/**
+ * binary_tree_node - Function to create a new node of binary tree
+ * @parent: Pointer to parent node
+ * @value: Value inside node
+ * Return: new node if it is created or null if it fails
+ */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
@@ -8,12 +15,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
 
-	return new_node;
+	return (new_node);
 }
