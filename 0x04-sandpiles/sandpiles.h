f96aea1931d071b6edfe458beb0550a61d9a8647
natyarteagac <natalia.arteagac94@gmail.com>
2022-01-13 23:58:50 -0500

    Sandpile

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -6,5 +6,6 @@
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 int stable(int grid[3][3]);
 void topple(int gridfirst[3][3], int gridsecond[3][3]);
+void add_sandpile(int grid1[3][3], int grid2[3][3]);
 
 #endif /* _SANDPILES_H_ */
