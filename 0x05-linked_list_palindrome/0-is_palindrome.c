f82ebd98311fec42fc1665555059b5c0f891d2af
natyarteagac <natalia.arteagac94@gmail.com>
2022-01-25 18:23:16 -0500

    Fixing issues of code

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 
 /**
- * @brief
- *
- * @param head
- * @return int
+ * is_palindrome - Function to verify if linked list is palindrome.
+ * @head: pointer to linked list head.
+ * Return: Value of function
  */
 
 int is_palindrome(listint_t **head)
@@ -13,25 +12,29 @@ int is_palindrome(listint_t **head)
 }
 
 /**
- * @brief
- *
+ * is_palindrome_util - Function to traverse the list.
+ * @left: pointer to left sido of linked list.
+ * @right: pointer to right side of linked list.
+ * Return: Zero if is palindrome or 1 if is not.
  */
 
 int is_palindrome_util(listint_t **left, listint_t *right)
 {
+	int isp;
+	int isp1;
 
 	if (right == 0)
 	{
 		return (1);
 	}
 
-	int isp = is_palindrome_util(left, right->next);
+	isp = is_palindrome_util(left, right->next);
 	if (isp == 0)
 	{
-		return 0;
+		return (0);
 	}
 
-	int isp1 = (right->n == (*left)->n);
+	isp1 = (right->n == (*left)->n);
 
 	*left = (*left)->next;
 
